e2ea8b074ecfc69d7ba43fdc66ae0d3197c42044
glen.wang@gmail.com <glen.wang@gmail.com>
2020-10-07 18:25:21 +0000

    name the datasource

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -3,17 +3,23 @@ package io.pivotal.pal.tracker;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.java.AbstractCloudConfig;
+//import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 
 import javax.sql.DataSource;
 
 @SpringBootApplication
-public class PalTrackerApplication  extends AbstractCloudConfig {
+public class PalTrackerApplication  {//extends AbstractCloudConfig {
 
     public static void main(String[] args) {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
+
+//    @Bean
+//    public DataSource dataSource() {
+//        return connectionFactory().dataSource("tracker-database");
+//    }
+
     @Bean
     public TimeEntryRepository getTimeEntryRepository(@Qualifier("tracker-database") DataSource datasources) {
         return new JdbcTimeEntryRepository(datasources);
